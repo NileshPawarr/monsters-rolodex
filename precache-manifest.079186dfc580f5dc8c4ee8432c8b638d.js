01bfcaa2a210af1545e297519cee627cb2444e5e
Nilesh Pawar <nileshpawar9922@gmail.com>
2021-06-02 20:27:26 +0530

    Updates

---
+++ precache-manifest.079186dfc580f5dc8c4ee8432c8b638d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4f7db1ebbb45161b1e000f874f43eb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "01b670911e20f20779e8",
+    "url": "/monsters-rolodex/static/css/main.8445548c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b670911e20f20779e8",
+    "url": "/monsters-rolodex/static/js/main.051e088c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
